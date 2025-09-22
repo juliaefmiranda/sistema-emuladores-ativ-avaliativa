50d43e9b3f8f342d8954f8c898d543863b1ceda3
julia <jerlo180908@gmail.com>
2025-09-22 13:57:52 -0300

    Cria rotas getAll e getById

--- src/controllers/emuladoresController.js
+++ src/controllers/emuladoresController.js
@@ -4,12 +4,30 @@ const { emuladores } = dados;
 const getAllEmuladores = (req, res) => {
     let resultado = emuladores; 
 
+    //Filtros
+
     res.status(200).json({
         total: resultado.length,
         data: resultado
     });
 }
 
+const getEmuladoresById = (req, res) => {
+    const id = parseInt(req.params.id);
 
+    const emulador = emuladores.find(e => e.id === id);
 
-export { getAllEmuladores }
+    if (!emulador) {
+        res.status(400).json({
+            success: false,
+            message: `Emulador com id ${id} não encontrado!`
+        });
+    }
+
+    res.status(200).json({
+        total: emulador.length,
+        data: emulador
+    });
+}
+
+export { getAllEmuladores, getEmuladoresById }
--- src/routes/emuladoresRoutes.js
+++ src/routes/emuladoresRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { getAllEmuladores } from "../controllers/emuladoresController.js";
+import { getAllEmuladores, getEmuladoresById } from "../controllers/emuladoresController.js";
 
 const router = express.Router();
 router.get("/", getAllEmuladores);
+router.get("/:id", getEmuladoresById);
 
 export default router;
